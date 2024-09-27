8a32e5f562ddf4c796cc39b0382937c9b6b65f8a
Valentin <52529145+YunaBlood@users.noreply.github.com>
2024-09-27 20:48:46 +0200

    Removed console.log not longer necessary

--- script.js
+++ script.js
@@ -15,13 +15,6 @@ function CreateGrid(rows, columns){
             createDivRows.appendChild(createDivColumns);
         }
     }
-    const divCountRows = document.querySelectorAll(".grid-rows")
-    const divCountColumn = document.querySelectorAll(".grid-columns")
-    const count_rows = divCountRows.length;
-    const count_columns = divCountColumn.length;
-    console.log("Number of Rows:",count_rows);
-    console.log("Number of Column:",count_columns);
-
 }
 
 CreateGrid();
@@ -35,8 +28,6 @@ function setHoverEffect(grid){
             divHover[grid].setAttribute("style", `background-color:grey`);
         });
     }
-    console.log(divHover)
-    console.log(grid)
 }
 
 setHoverEffect();
